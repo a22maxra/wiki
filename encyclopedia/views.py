827dfb22d98659d99304360de1953afdb0d9186e
maxx6956 <max.rapp01@gmail.com>
2022-05-19 19:16:24 +0200

    first commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django import forms
+
+from . import util
+
+class EntryForm(forms.Form):
+    entry = forms.CharField(label="Title")
+
+
+def index(request):
+    ## if method is post search
+    query = request.GET.get("q")
+    entries = util.list_entries()
+    title = "All Pages"
+    # If query has a value method is post
+    if query:
+        print(query)
+        # If search matches an entry, go to url otherwise show all matches
+        if query in entries:
+            return redirect("entries", query)
+        else:
+            entries = [match for match in entries if query in match]
+            title = "Search Results"
+    return render(request, "encyclopedia/index.html", {
+        "entries": entries,
+        "title": title
+    })
+
+
+def entries(request, entry):
+    # Compare the url with all the entries, if it's valid load the page.
+    currentEntry = util.get_entry(entry)
+    if currentEntry:
+        return HttpResponse(currentEntry)
+    else:
+        return HttpResponse(f"{entry} is not a valid entry.")
+
+
+def create(request):
+    return render(request, "encyclopedia/create.html", {
+        "form": EntryForm()
+    })
